22fcc3376f7d00ae64b7555a5e18bd75a67e9d73
MALIKA872 <malikamaharani04@gmail.com>
2024-11-17 19:03:17 +0800

    first commit

---
+++ MinGW/bin/node-jiti/filament-tailwind.config.js.98d0e6e6.js
@@ -0,0 +1,7 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _tailwindconfigpreset = require('./tailwind.config.preset'); var _tailwindconfigpreset2 = _interopRequireDefault(_tailwindconfigpreset);
+
+exports. default = {
+    presets: [_tailwindconfigpreset2.default],
+    content: ['./packages/**/*.blade.php'],
+}
+ /* v7-246294201881bf28 */
